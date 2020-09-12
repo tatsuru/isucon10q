d839b8254bfaa7f8dd782c8139ed93dd9825308d
polamjag <s@polamjag.info>
2020-09-12 19:06:21 +0900

    バルクインサート一発なのでトランザクション不要

--- isuumo/webapp/go/main.go
+++ isuumo/webapp/go/main.go
@@ -363,13 +363,6 @@ func postChair(c echo.Context) error {
 
 	c.Logger().Debugf("postChair: start process %d rows", len(records))
 
-	tx, err := db.Begin()
-	if err != nil {
-		c.Logger().Errorf("failed to begin tx: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	defer tx.Rollback()
-
 	toInsert := make([]Chair, len(records))
 	for _, row := range records {
 		rm := RecordMapper{Record: row}
@@ -406,13 +399,9 @@ func postChair(c echo.Context) error {
 			Stock:       int64(stock),
 		})
 	}
-	_, err = tx.NamedExec("INSERT INTO chair(id, name, description, thumbnail, price, height, width, depth, color, features, kind, popularity, stock) VALUES(:id, :name, :description, :thumbnail, :price, :height, :width, :depth, :color, :features, :kind, :popularity, :stock)", toInsert)
+	_, err = db.NamedExec("INSERT INTO chair(id, name, description, thumbnail, price, height, width, depth, color, features, kind, popularity, stock) VALUES(:id, :name, :description, :thumbnail, :price, :height, :width, :depth, :color, :features, :kind, :popularity, :stock)", toInsert)
 	if err != nil {
 		c.Logger().Errorf("failed to insert chair: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	if err := tx.Commit(); err != nil {
-		c.Logger().Errorf("failed to commit tx: %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
 	return c.NoContent(http.StatusCreated)
